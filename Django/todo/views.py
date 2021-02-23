80e63e0825e2ee7008b5b58dac56c93c098f736c
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-23 18:40:00 +0100

    created Class Views and function views to handle the incoming GET and POST requests

--- Django/todo/views.py
+++ Django/todo/views.py
@@ -2,11 +2,48 @@ from django.http import HttpResponse, response
 from django.http import HttpResponseRedirect
 from django.utils import html
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+
+from .models import Task
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'todo/home.html')
+    data = Task.objects.values()
+    print(len(data))
+    for i in data:
+        print(i)
+        id = i['id']
+        task = i['task']
+        task_description = i['task_description']
+    context = {'data' : data}
+    return render(request, 'todo/home.html', context)
+
+class Edit(View):
+    def get(request, id):
+
+
+        return render(request, 'todo/edit.html')
+
+    def get(request):
+        pass
+
+class New(View):
+    def get(self, request):
+        return render(request, 'todo/new.html')
+
+    def post(self, request):
+        response_task = request.POST.get('task')
+        response_task_description = request.POST.get('task_description')
+        t = Task(task=response_task, task_description=response_task_description)
+        t.save()
+        return HttpResponseRedirect('success')
+
+
+class Success(View):
+    def get(self, request):
+        return render(request, 'todo/success.html')
+
+

9c6de5a7e08ee1c4008235e2235d5e96a1a4be3b
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-23 14:34:11 +0100

    create the model for the todo app

--- Django/todo/models.py
+++ Django/todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    task = models.CharField(max_length=256)
+    task_description = models.CharField(max_length=400)

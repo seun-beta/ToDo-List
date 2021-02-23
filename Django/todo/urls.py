33e7866252b52746fa64b042eaf848ea436de81a
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-23 18:39:17 +0100

    added index, success and add new item url

--- Django/todo/urls.py
+++ Django/todo/urls.py
@@ -5,7 +5,10 @@ from . import views
 
 app_name = 'todo'
 
-INSTALLED_APPS = [
-    path('', views.index, name='index')
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('edit/<int:id>', views.Edit.as_view(), name='edit'),
+    path('new', views.New.as_view(), name='new'),
+    path('success', views.Success.as_view(), name='success')
 
 ]

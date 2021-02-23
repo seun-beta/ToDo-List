03094769fb7dfb5d1e71c40feaa0911e0adc6c10
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-23 14:34:59 +0100

    create index/home view for todo app

--- Django/todo/views.py
+++ Django/todo/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse, response
+from django.http import HttpResponseRedirect
+from django.utils import html
+from django.views import View
 from django.shortcuts import render
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'todo/home.html')

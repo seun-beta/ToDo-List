73c3d0043b21acd9ae1329a29edbec6fec325910
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-02-28 20:35:21 +0100

    removed task_description class variable from Task class

--- Django/todo/models.py
+++ Django/todo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Task(models.Model):
     task = models.CharField(max_length=256)
-    task_description = models.CharField(max_length=400)
+    #task_description = models.CharField(max_length=400)
 
     #def __str__(self):
     #    return self.task, self.task_description

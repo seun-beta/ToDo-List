dd4723bc49595d82b6ce46200c3acb489860ea27
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-03 15:10:35 +0100

    removed unused import statements and added ListView as the index view

--- Django/todo/views.py
+++ Django/todo/views.py
@@ -1,28 +1,34 @@
-from django.http import HttpResponse, response
-from django.http import HttpResponseRedirect
-from django.utils import html
-from django.views import View
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
 from django.views import View
-from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+
 
 from .models import Task
 
 # Create your views here.
 
-def index(request):
-    data = Task.objects.values()
-    context = {'data' : data}
-    return render(request, 'todo/home.html', context)
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'my_tasks'
+    template_name = 'todo/task_list.html'
     
 class Edit(View):
-    def get(request, id):
+    def get(self, request, number):
+        task = Task.objects.get(pk=number)
+        context = {'task' : task }
 
+        return render(request, 'todo/edit.html', context)
 
-        return render(request, 'todo/edit.html')
+    def post(self, request, number):
+        edit_task = request.POST.get('edit_task')
+        #task_id = request.POST.get('task_id')
+        edited_task = Task.objects.filter(pk=number).update(task=edit_task)
+        #edited_task.save()
 
-    def get(request):
-        pass
+        return HttpResponse(request, 'todo/edit_success.html')
+        
 
 class New(View):
     def get(self, request):
@@ -30,8 +36,8 @@ class New(View):
 
     def post(self, request):
         response_task = request.POST.get('task')
-        t = Task(task=response_task)
-        t.save()
+        task = Task(task=response_task)
+        task.save()
         return HttpResponseRedirect('success')
 
 

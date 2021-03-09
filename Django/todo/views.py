d8927d9be6427347afe0e269fc7f0ad46c974563
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-09 01:54:52 +0100

    changed views to generic Create, Update & Delete

--- Django/todo/views.py
+++ Django/todo/views.py
@@ -1,49 +1,32 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic import ListView
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.views import View
-from django.urls import reverse
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
+
+from todo.models import Task
+
+class HomeView(View):
+    def get(self, request):
+        task = Task.objects.all()
+        ctx = { 'task_list' : task }
+        return render(request, 'todo/home.html', ctx)
 
 
-from .models import Task
-
-class TaskList(ListView):
+class TaskNew(CreateView):
     model = Task
-    context_object_name = 'my_tasks'
-    template_name = 'todo/task_list.html'
+    fields = '__all__'
+    success_url = reverse_lazy('todo:home')
     
-class Edit(View):
-    def get(self, request, number):
-        task = Task.objects.get(pk=number)
-        context = {'task' : task }
-
-        return render(request, 'todo/edit.html', context)
-
-    def post(self, request, number):
-        if request.POST.get('submit_task'):
-            edit_task = request.POST.get('edit_task')
-            edited_task = Task.objects.filter(pk=number).update(task=edit_task)
-
-        elif request.POST.get('delete_task'):
-            delete_task = request.POST.get('edit_task')
-            delete_task = Task.objects.filter(pk=number).delete()
 
 
-
-        return render(request, 'todo/edit_success.html')
-        
-
-class New(View):
-    def get(self, request):
-        return render(request, 'todo/new.html')
-
-    def post(self, request):
-        response_task = request.POST.get('task')
-        task = Task(task=response_task)
-        task.save()
-        return render(request, 'todo/new_success.html')
+class TaskEdit(UpdateView):
+    model = Task
+    success_url = reverse_lazy('todo:home')
+    fields = ['task', 'description']
 
 
-class Success(View):
-    def get(self, request):
-        return render(request, 'todo/success.html')
+class TaskDelete(DeleteView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('todo:home')
+

0c20ebfb7d7425a7d8170a77477c5018ab6de6eb
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-09 01:54:13 +0100

    refactored code and used new class based views

--- Django/todo/urls.py
+++ Django/todo/urls.py
@@ -1,14 +1,11 @@
-from core.settings import INSTALLED_APPS
-from django.urls import path
-from django.views.generic import TemplateView
 from . import views
+from django.urls import path
 
 app_name = 'todo'
 
 urlpatterns = [
-    path('', views.TaskList.as_view(), name='index'),
-    path('edit/<int:number>', views.Edit.as_view(), name='edit'),
-    path('new', views.New.as_view(), name='new'),
-    path('success', views.Success.as_view(), name='success')
-
+    path('', views.HomeView.as_view(), name='home'),
+    path('new', views.TaskNew.as_view(), name='new'),
+    path('delete/<int:pk>', views.TaskDelete.as_view(), name='delete'),
+    path('edit/<int:pk>', views.TaskEdit.as_view(), name='edit'),
 ]

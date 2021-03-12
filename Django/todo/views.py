b7b69b76194a2b014664b734effd33a4df5f12a0
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-12 16:08:24 +0100

    changed names of generic views

--- Django/todo/views.py
+++ Django/todo/views.py
@@ -5,28 +5,27 @@ from django.urls import reverse_lazy
 
 from todo.models import Task
 
+
 class HomeView(View):
     def get(self, request):
         task = Task.objects.all()
-        ctx = { 'task_list' : task }
+        ctx = {'task_list': task}
         return render(request, 'todo/home.html', ctx)
 
 
-class TaskNew(CreateView):
-    model = Task
-    fields = '__all__'
-    success_url = reverse_lazy('todo:home')
-    
-
-
-class TaskEdit(UpdateView):
-    model = Task
-    success_url = reverse_lazy('todo:home')
-    fields = ['task', 'description']
-
-
-class TaskDelete(DeleteView):
+class NewTask(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('todo:home')
 
+
+class EditTask(UpdateView):
+    model = Task
+    success_url = reverse_lazy('todo:home')
+    fields = '__all__'
+
+
+class DeleteTask(DeleteView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('todo:home')

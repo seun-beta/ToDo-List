74f47e7f2e43a338a91480abd741b883ef95cd7e
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-12 16:01:55 +0100

    refactored to ensure PEP8 compliance

--- Django/todo/models.py
+++ Django/todo/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 
+
 class Task(models.Model):
     task = models.CharField(
         max_length=200,
         help_text='Enter a task',
-        validators = [MinLengthValidator(1, "Task must be greater than 1 character")]
+        validators=[MinLengthValidator(1,
+                    "Task must be greater than 1 character")]
     )
     description = models.CharField(
         max_length=500,
-        help_text= "Enter the description of your task"  
-    )
+        help_text="Enter the description of your task")
 
     def __str__(self):
         return self.task
